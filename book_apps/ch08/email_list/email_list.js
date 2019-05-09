4e6caa30bfc34eecb9cf7641233c8b8bb8887ed7
shieldgenerator7 <shieldgenerator7@yahoo.com>
2019-05-08 23:50:40 -0400

    updates during class 2019-05-08

--- book_apps/ch08/email_list/email_list.js
+++ book_apps/ch08/email_list/email_list.js
@@ -6,34 +6,34 @@ $(document).ready(function() {
         var isValid = true;
 
         // validate the first email address
-        if (emailAddress1 === "") { 
+        if (emailAddress1 === "") {
             $("#email_address1").next().text("This field is required.");
             isValid = false;
         } else {
             $("#email_address1").next().text("");
-        } 
-        
+        }
+        $("#last_name").next().text()
         // validate the second email address
-        if (emailAddress2 === "") { 
+        if (emailAddress2 === "") {
             $("#email_address2").next().text("This field is required.");
-            isValid = false; 
-        } else if (emailAddress1 !== emailAddress2) { 
+            isValid = false;
+        } else if (emailAddress1 !== emailAddress2) {
             $("#email_address2").next().text(
                 "This entry must equal first entry.");
             isValid = false;
         } else {
             $("#email_address2").next().text("");
         }
-        
-        // validate the first name entry  
+
+        // validate the first name entry
         if ($("#first_name").val() === "") {
             $("#first_name").next().text("This field is required.");
             isValid = false;
-        } 
+        }
         else {
             $("#first_name").next().text("");
         }
-        
+
         // submit the form if all entries are valid
         if (isValid) {
             $("#email_form").submit();
--- book_apps/ch08/faqs/faqs.js
+++ book_apps/ch08/faqs/faqs.js
@@ -1,5 +1,6 @@
 "use strict";
 $(document).ready(function() {
+  $("div:parent").parent().find("#last_name").text("killed");
     $("#faqs h2").click(function(evt) {
         $(this).toggleClass("minus");
         if ($(this).attr("class") !== "minus") {
